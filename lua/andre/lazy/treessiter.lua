1a96e8e61365fa70238b49bedb1c5f29bedbf813
arbezerra <arbezerra@gmail.com>
2024-02-23 17:32:54 -0300

    fix: tressiter

--- lua/andre/lazy/treessiter.lua
+++ lua/andre/lazy/treessiter.lua
@@ -6,5 +6,14 @@ return {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		build = ":TSUpdate",
+		config = function()
+			local treesitter = require("nvim-treesitter.configs")
+
+			treesitter.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
+				sync_install = false,
+				highlight = { enable = true },
+			})
+		end,
 	},
 }
